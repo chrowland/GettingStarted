201bbb18069387abf1a50fd238fc57374301615d
chrowland <chrisrow@zillowgroup.com>
2024-09-14 10:49:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd
 
+# Set the title and favicon that appear in the Browser's tab bar.
+st.set_page_config(
+    page_title="RAPTOR - PA",
+    page_icon=":eagle:",  # This is an emoji shortcode. Could be a URL too.
+)
 st.title("PA RAPTOR")
 st.write("""
 # My first app
@@ -8,6 +13,7 @@ Let's Go!
 """)
  
 df = pd.read_csv("Flex_24.csv")
+df.set_index('metric', inplace=True)
 st.dataframe(df)
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
