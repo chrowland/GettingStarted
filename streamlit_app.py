b811af93943683fa8c9d06d59177f633179c26e5
chrowland <chrisrow@zillowgroup.com>
2024-09-13 21:18:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
 st.title("🎈 My new app")
+st.write("""
+# My first app
+Hello *world!*
+""")
+ 
+df = pd.read_csv("my_data.csv")
+st.line_chart(df)
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
