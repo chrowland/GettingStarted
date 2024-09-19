2f748de0c138fd09707d9bcbafda3d7de81080d4
chrowland <chrisrow@zillowgroup.com>
2024-09-19 18:06:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,8 +104,8 @@ st.data_editor(
     disabled=["R&O"],
     hide_index=True,
 )
-chart2=alt.Chart(chartdf.reset_index()).mark_bar().encode(
-  x=alt.X('index',sort=None),
-  y='Baseline','Scenario'
-)
-st.altair_chart(chart2)
+#chart2=alt.Chart(chartdf.reset_index()).mark_bar().encode(
+#  x=alt.X('index',sort=None),
+#  y='Baseline','Scenario'
+#)
+#st.altair_chart(chart2)
