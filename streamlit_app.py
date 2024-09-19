a436e07b95fd7d7b7e8f2e6f769bbafc765d4ff1
chrowland <chrisrow@zillowgroup.com>
2024-09-19 14:54:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ chart=alt.Chart(sensitivities).mark_bar().encode(
     x='Impact:Q',
     y='Metric:O'
 )
-st.altair_chart(chart)
+st.altair_chart(chart,stack=False)
 
 st.dataframe(sensitivities.sort_values('Impact',ascending=False))
 st.title("Risks and Opportunities")
