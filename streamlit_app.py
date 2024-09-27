1ad5aefe636923ad437a7daaf6b5edfe1d0258b3
chrowland <chrisrow@zillowgroup.com>
2024-09-27 15:07:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ Final_Frame=df.T.join(output.T,how='left')
 #scenario_name=scenario_form.text_input('Name your Scenario:', 'Danny\'s super cool scenario')
 #submit=scenario_form.form_submit_button('Done')
 with st.form("scenario_form"):
-    st.text_input('Name your Scenario:','Danny\'s super cool scenario')
+    scenario_name=st.text_input('Name your Scenario:','Danny\'s super cool scenario')
     st.form_submit_button('Submit')
     st.download_button("Download this scenario",Final_Frame.T.to_csv(),f"{scenario_name}.csv",use_container_width=True)
 #st.download_button("Push Scenario to Hive",Final_Frame.T.to_csv(),f"{scenario_name}.csv",use_container_width=True)
